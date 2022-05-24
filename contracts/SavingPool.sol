0929c29b5148432017d61dbb176c40165079518c
Joaquin Repetto <joaquinrepetto@MacBook-Pro-de-Joaquin.local>
2022-05-24 14:12:43 -0300

    initial repo

--- contracts/SavingPool.sol
+++ contracts/SavingPool.sol
@@ -15,15 +15,9 @@ contract SavingPool {
     mapping(address => uint256) public balances;
     string public purpose = "El primero";
 
-    // the mainnet AAVE v2 lending pool
-    // IPool pool = IPool(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9);
-
     // the polygon mainnet AAVE v3 lending pool
     IPool pool = IPool(0x794a61358D6845594F94dc1DB02A252b5b4814aD);
     IPoolDataProvider dataProvider;
-
-    // aave interest bearing DAI (V2)
-    // IERC20 aDai = IERC20(0x028171bCA77440897B824Ca71D1c56caC55b68A3);
 
     // aave interest bearing polygon DAI (V3)
     IERC20 aDai = IERC20(0x82E64f49Ed5EC1bC6e43DAD4FC8Af9bb3A2312EE);
@@ -42,59 +36,18 @@ contract SavingPool {
     }
 
     function depositDai(uint256 _amount) public returns (bool success) {
-        require(
-            block.timestamp <= startDate,
-            "Inscripcion no disponible, inversion en curso"
-        );
-        require((_amount > 0), "Amount must be greater than 0");
-
-        success = dai.transferFrom(msg.sender, address(this), _amount);
-        require(success, "Failed to transfer DAI");
-        balances[msg.sender] += _amount;
-
         return success;
     }
 
-    function transferFundsToPool() public {
-        require(msg.sender == owner, "Only owner can transfer funds to pool");
-        require(block.timestamp > startDate, "Inscripcion aun abierta");
-
-        totalDeposit = dai.balanceOf(address(this));
-
-        dai.approve(address(pool), totalDeposit);
-        pool.supply(address(dai), totalDeposit, address(this), 0);
-        endDate = block.timestamp + 21 seconds;
-    }
+    function transferFundsToPool() public {}
 
     function getInterestRate()
         public
         view
         returns (DataTypes.ReserveData memory reserveData)
-    {
-        // uint256 totalBalance = aDai.balanceOf(address(this));
-        reserveData = pool.getReserveData(address(dai));
-        return reserveData;
-    }
+    {}
 
-    function withdraw() public {
-        require(msg.sender == owner, "Only owner can withdraw");
-        require(block.timestamp > endDate, "Inversion aun en curso");
-        aDai.approve(address(pool), type(uint256).max);
-        pool.withdraw(address(dai), type(uint256).max, address(this));
-    }
+    function withdraw() public {}
 
-    function reclaimInteret() public {
-        require(block.timestamp > endDate, "Inversion aun en curso");
-        require(
-            dai.balanceOf(address(this)) > 0,
-            "No hay intereses a reclamar"
-        );
-        uint256 sharePercent = balances[msg.sender] / totalDeposit;
-        uint256 interest = sharePercent *
-            sharePercent *
-            dai.balanceOf(address(this));
-
-        console.log("Deposito", sharePercent, "Interes:", interest);
-        dai.transfer(msg.sender, interest);
-    }
+    function reclaimInteret() public {}
 }

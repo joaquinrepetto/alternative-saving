498b60f2acbac83c1a770655efe19645846644b8
Joaquin Repetto <joaquinrepetto@MacBook-Pro-de-Joaquin.local>
2022-05-25 19:58:00 -0300

    remove submodule

---
+++ frontend/src/containers/Main.jsx
@@ -0,0 +1,150 @@
+import "antd/dist/antd.css";
+import React, { useCallback, useEffect, useState } from "react";
+import { Account } from "../components";
+import {
+  useGasPrice,
+  useUserProviderAndSigner,
+  useContractLoader,
+  useContractReader,
+} from "eth-hooks";
+import { NETWORKS, ALCHEMY_KEY } from "../constants";
+import externalContracts from "../contracts/external_contracts";
+import deployedContracts from "../contracts/hardhat_contracts.json";
+import { Transactor, Web3ModalSetup } from "../helpers";
+import { useStaticJsonRPC, useContractConfig } from "../hooks";
+
+const { ethers } = require("ethers");
+const initialNetwork = NETWORKS.localhost; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
+
+const DEBUG = true;
+const NETWORKCHECK = true;
+const USE_BURNER_WALLET = true; // toggle burner wallet feature
+const USE_NETWORK_SELECTOR = false;
+
+const web3Modal = Web3ModalSetup();
+
+// 🛰 providers
+const providers = [
+  "https://eth-mainnet.gateway.pokt.network/v1/lb/611156b4a585a20035148406",
+  `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+  "https://rpc.scaffoldeth.io:48544",
+];
+
+function Main() {
+  const networkOptions = [initialNetwork.name, "mainnet", "rinkeby"];
+
+  const [injectedProvider, setInjectedProvider] = useState();
+  const [address, setAddress] = useState();
+  const [selectedNetwork, setSelectedNetwork] = useState(networkOptions[0]);
+
+  const targetNetwork = NETWORKS[selectedNetwork];
+
+  // 🔭 block explorer URL
+  const blockExplorer = targetNetwork.blockExplorer;
+
+  // load all your providers
+  const localProvider = useStaticJsonRPC([
+    process.env.REACT_APP_PROVIDER
+      ? process.env.REACT_APP_PROVIDER
+      : targetNetwork.rpcUrl,
+  ]);
+  const mainnetProvider = useStaticJsonRPC(providers);
+
+  if (DEBUG) console.log(`Using ${selectedNetwork} network`);
+
+  // 🛰 providers
+  if (DEBUG) console.log("📡 Connecting to Mainnet Ethereum");
+
+  const logoutOfWeb3Modal = async () => {
+    await web3Modal.clearCachedProvider();
+    if (
+      injectedProvider &&
+      injectedProvider.provider &&
+      typeof injectedProvider.provider.disconnect == "function"
+    ) {
+      await injectedProvider.provider.disconnect();
+    }
+    setTimeout(() => {
+      window.location.reload();
+    }, 1);
+  };
+
+  const userProviderAndSigner = useUserProviderAndSigner(
+    injectedProvider,
+    localProvider
+  );
+  const userSigner = userProviderAndSigner.signer;
+
+  useEffect(() => {
+    async function getAddress() {
+      if (userSigner) {
+        const newAddress = await userSigner.getAddress();
+        setAddress(newAddress);
+      }
+    }
+    getAddress();
+  }, [userSigner]);
+
+  /* 🔥 This hook will get the price of Gas from ⛽️ EtherGasStation */
+  const gasPrice = useGasPrice(targetNetwork, "fast");
+
+  // The transactor wraps transactions and provides notificiations
+  const tx = Transactor(userSigner, gasPrice);
+
+  const contractConfig = useContractConfig();
+
+  // const contractConfig = {
+  //   deployedContracts: deployedContracts || {},
+  //   externalContracts: externalContracts || {},
+  // };
+
+  // Load in your local 📝 contract and read a value from it:
+  const readContracts = useContractLoader(localProvider, contractConfig);
+
+  // If you want to make 🔐 write transactions to your contracts, use the userSigner:
+  const writeContracts = useContractLoader(userSigner, contractConfig);
+
+  const mainnetContracts = useContractLoader(mainnetProvider, contractConfig);
+
+  // Then read your DAI balance like:
+  const myMainnetDAIBalance = useContractReader(
+    mainnetContracts,
+    "DAI",
+    "balanceOf",
+    ["0x34aA3F359A9D614239015126635CE7732c18fDF3"]
+  );
+
+  // keep track of a variable from the contract in the local React state:
+  const purpose = useContractReader(readContracts, "SavingPool", "purpose");
+
+  const loadWeb3Modal = useCallback(async () => {
+    const provider = await web3Modal.connect();
+    setInjectedProvider(new ethers.providers.Web3Provider(provider));
+
+    provider.on("chainChanged", (chainId) => {
+      console.log(`chain changed to ${chainId}! updating providers`);
+      setInjectedProvider(new ethers.providers.Web3Provider(provider));
+    });
+
+    provider.on("accountsChanged", () => {
+      console.log(`account changed!`);
+      setInjectedProvider(new ethers.providers.Web3Provider(provider));
+    });
+
+    // Subscribe to session disconnection
+    provider.on("disconnect", (code, reason) => {
+      console.log(code, reason);
+      logoutOfWeb3Modal();
+    });
+  }, [setInjectedProvider]);
+
+  useEffect(() => {
+    if (web3Modal.cachedProvider) {
+      loadWeb3Modal();
+    }
+  }, [loadWeb3Modal]);
+
+  return <div>asd</div>;
+}
+
+export default Main;
